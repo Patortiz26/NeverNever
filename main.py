f651b389c712da2a0e5e00e2b246c71e6255e371
Patortiz26 <89476610+Patortiz26@users.noreply.github.com>
2023-04-22 13:31:13 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import streamlit as st
+
+if __name__ == "__main__":
+    st.set_page_config(page_title="Nunca nunca")
+    st.title('Nunca nunca')
+    df = pd.read_csv('bd.csv')
+
+    n = st.number_input("Ingresa el número de fila que quieres ver", min_value=0, max_value=len(df), step=1
+                        )
+    if n!=0:
+        st.markdown("""
+        <style>
+        .big-font {
+            font-size:100px !important;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+        st.markdown(f'<p class="big-font">{df.iloc[n-1,1]}</p>', unsafe_allow_html=True)
+        # st.write(, style={'fontSize':'48pt'})
